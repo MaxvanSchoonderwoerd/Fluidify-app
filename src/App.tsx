dc917170b8c305d7d99a5e3478c1fe983ffa8dc7
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2024-05-28 14:28:05 +0200

    added counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [fluidBalance, setFluidBalance] = useState<number>(0);
+
+  const addFluid = (ammount: number) => {
+    setFluidBalance(fluidBalance + ammount);
+  };
+
+  const removeFluid = (ammount: number) => {
+    if (fluidBalance >= ammount) {
+      setFluidBalance(fluidBalance - ammount);
+    } else {
+      setFluidBalance(0);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Balance: {fluidBalance}ml</p>
+      <p>Blikje (330ml)</p>
+      <button onClick={() => removeFluid(330)}>-</button>
+      <button onClick={() => addFluid(330)}>+</button>
     </div>
   );
 }
