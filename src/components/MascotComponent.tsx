49c33dda8c2cf19801071818f7e6839bd4c39386
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2024-06-04 13:40:33 +0200

    refactor

--- src/components/MascotComponent.tsx
+++ src/components/MascotComponent.tsx
@@ -5,6 +5,8 @@ import image3 from "../assets/smily3.png";
 import image4 from "../assets/smily4.png";
 import image5 from "../assets/smily5.png";
 
+const images = [image1, image2, image3, image4, image5];
+
 type TMascotComponentProps = {
   totalFluidBalance: number;
 };
@@ -13,35 +15,25 @@ export default function MascotComponent(props: TMascotComponentProps) {
   const [mascotImage, setMascotImage] = useState<string>();
 
   const fluidLimit: number = 2000;
+  const switchMascotImage = () => {
+    const part = fluidLimit / images.length;
+    images.forEach((image, i) => {
+      if (props.totalFluidBalance >= part * i && props.totalFluidBalance < part * (i + 1)) {
+        setMascotImage(image);
+      }
+    });
+  };
 
   useEffect(() => {
-    const quartile = fluidLimit / 4;
-
-    switch (true) {
-      case props.totalFluidBalance <= 0:
-        setMascotImage(image1);
-        break;
-      case props.totalFluidBalance >= quartile * 1 && props.totalFluidBalance < quartile * 2:
-        setMascotImage(image2);
-        break;
-      case props.totalFluidBalance >= quartile * 2 && props.totalFluidBalance < quartile * 3:
-        setMascotImage(image3);
-        break;
-      case props.totalFluidBalance >= quartile * 3 && props.totalFluidBalance < fluidLimit:
-        setMascotImage(image4);
-        break;
-      case props.totalFluidBalance >= fluidLimit:
-        setMascotImage(image5);
-        break;
-      default:
-        setMascotImage(image1); // Fallback
-        break;
-    }
-  }, [props.totalFluidBalance, fluidLimit]);
+    switchMascotImage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.totalFluidBalance]);
 
   return (
     <>
-      <h1 className="font-semibold text-4xl mt-4 mb-4">Balans: {props.totalFluidBalance}ml</h1>
+      <h1 className="font-semibold text-4xl mt-4 mb-4">
+        Balans: {props.totalFluidBalance}ml / {fluidLimit}ml
+      </h1>
       <img src={mascotImage} alt="mascot" />
     </>
   );
