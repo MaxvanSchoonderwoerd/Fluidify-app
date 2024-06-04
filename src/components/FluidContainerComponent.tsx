b551062c933556bb186bfb3bccd7714a5588c7e5
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2024-06-04 14:07:10 +0200

    added comments

--- src/components/FluidContainerComponent.tsx
+++ src/components/FluidContainerComponent.tsx
@@ -27,15 +27,19 @@ export default function FluidContainerComponent(props: TFluidContainerProps) {
 
   return (
     <div className="bg-blue-500 w-64 p-4 m-4 grid place-items-center rounded-lg relative">
+      {/* if the patient drank the container, add a badge with the ammount */}
       {ammount > 0 && (
         <div className="bg-red-600  absolute -right-4 -top-4 size-8 rounded-full text-center flex justify-center items-center">
           <p className="text-white">{ammount}</p>
         </div>
       )}
 
+      {/* container name and volume indicators */}
       <p className="font-medium text-white">
         {props.name} ({props.volume}ml)
       </p>
+
+      {/* buttons for adding or removing fluid volume by selecting how much of the container they drank */}
       <div className="w-full flex flex-row justify-between mt-2 ">
         <button className="bg-white size-10 text-blue-500 disabled:bg-gray-300 disabled:text-gray-500" onClick={() => handleButtonClicked("minus", 1)} disabled={ammount < 1}>
           - 1
